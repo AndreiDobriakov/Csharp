780b7950840b4bdb5366e88c2d5832dbf73470c8
Andrei <dabla2000@mail.ru>
2023-09-21 17:00:58 +0300

    решение четвертой задачи

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -3,6 +3,7 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+
 Console.WriteLine("Напишите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
@@ -12,7 +13,10 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Напишите третье число");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 > number2)
-{
-    
-}
+int numberMax = number1;
+
+if (number2 > numberMax) numberMax = number2;
+
+if (number3 > numberMax) numberMax = number3;
+
+Console.WriteLine($"Максимальное число из введенных равно {numberMax}");
