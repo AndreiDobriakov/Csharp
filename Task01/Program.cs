a9a2d218456a184ed15c2cdad24527f72a06be64
Andrei <dabla2000@mail.ru>
2023-09-22 06:36:42 +0300

    решение первой задачи

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -11,6 +11,7 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Напишите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
+
 if (number2 * number2 == number1)
 {
     Console.WriteLine("Первое число является квадратом второго");
