ae7d6e74ef1af03a86446f9d53f8c7962f16abaf
Andrei <dabla2000@mail.ru>
2023-09-26 08:29:48 +0300

    решение 14 задачи

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+int GetUserInput(string str)
+{
+    Console.Write($"{str}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+bool DivisionTo7end23(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+int number = GetUserInput("Введите число");
+
+bool result = DivisionTo7end23(number);
+Console.WriteLine(result ? $"{number} -> да" : $"{number} -> нет");
