5aaff1df2788e4f32531a143155a00e5e3054dd9
Andrei <dabla2000@mail.ru>
2023-10-06 10:58:23 +0300

    решение 38 задачи

---
+++ Task38/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] CreateArrayRandom(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return arr;
+}
+
+double FindMinNumber(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (min > arr[i]) min = arr[i];
+    }
+    return min;
+}
+
+double FindMaxNumber(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (max < arr[i]) max = arr[i];
+    }
+    return max;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]:F2}, ");
+        else Console.Write($"{arr[i]:F2}");
+    }
+    Console.WriteLine("]");
+}
+
+double[] array = CreateArrayRandom(10, 0, 10);
+PrintArray(array);
+double result = FindMaxNumber(array) - FindMinNumber(array);
+Console.WriteLine($"разницу между максимальным({FindMaxNumber(array):F2}) и минимальным({FindMinNumber(array):F2}) элементом массива равна {result:F1}");
